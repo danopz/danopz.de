d1b16e92258ac5d4a361c1b68e3a98509fb4aab8
Daniel Opitz <git@copynpaste.de>
2019-05-16 23:29:50 +0200

    add contributions

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+CHANGES=$(git diff --name-only)
+CHANGES_COUNT=0
+
+for i in ${CHANGES[@]}; do
+    CHANGES_COUNT=$((CHANGES_COUNT+1))
+done
+
+[ ${CHANGES_COUNT} -eq 0 ] || git stash
+git pull
+[ ${CHANGES_COUNT} -eq 0 ] || git stash pop
+
+git add contributions.json
+
+git commit -m"Automatic API data update"
+git push origin HEAD
