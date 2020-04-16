bd91d888623982e83c672e12c4a320d8621c29a5
Daniel Opitz <git@copynpaste.de>
2020-04-16 23:59:42 +0200

    refactor image create script

--- bin/create-images.js
+++ bin/create-images.js
@@ -3,21 +3,20 @@
 const path = require('path');
 const sharp = require('sharp');
 
-const srcBg = sharp(path.join(__dirname, '..', 'src', 'bg.jpg'));
-const srcMe = sharp(path.join(__dirname, '..', 'src', 'me.jpg'));
-const imagePath = path.join(__dirname, '..', 'public', 'img');
+const bgImg = sharp(path.join(__dirname, '..', 'src', 'bg.jpg')).jpeg({ quality: 30, chromaSubsampling: '4:2:0' });
+const profileImg = sharp(path.join(__dirname, '..', 'src', 'me.jpg'));
+const imgOutPath = path.join(__dirname, '..', 'public', 'img');
 
-const bgOptions = { quality: 30, chromaSubsampling: '4:2:0' };
 const extractIcon = { left: 850, top: 0, width: 1500, height: 1500 };
 
-srcMe.clone().resize(300, 400).jpeg({ quality: 70 }).toFile(path.join(imagePath, 'daniel-opitz.jpg'));
-srcMe.clone().extract(extractIcon).resize(200, 200).jpeg({ quality: 50 }).toFile(path.join(imagePath, 'daniel-opitz-icon.jpg'));
+profileImg.clone().resize(300, 400).jpeg({ quality: 70 }).toFile(path.join(imgOutPath, 'daniel-opitz.jpg'));
+profileImg.clone().extract(extractIcon).resize(200, 200).jpeg({ quality: 50 }).toFile(path.join(imgOutPath, 'daniel-opitz-icon.jpg'));
 
-srcBg.clone().resize(1920).jpeg(bgOptions).toFile(path.join(imagePath, 'bg-1920.jpg'));
-srcBg.clone().resize(1200).jpeg(bgOptions).toFile(path.join(imagePath, 'bg-1200.jpg'));
-srcBg.clone().resize(992).jpeg(bgOptions).toFile(path.join(imagePath, 'bg-992.jpg'));
+bgImg.clone().resize(1920).toFile(path.join(imgOutPath, 'bg-1920.jpg'));
+bgImg.clone().resize(1200).toFile(path.join(imgOutPath, 'bg-1200.jpg'));
+bgImg.clone().resize(992).toFile(path.join(imgOutPath, 'bg-992.jpg'));
 
-srcBg.metadata().then(function (m) {
+bgImg.metadata().then((m) => {
     const extractOptions = {
         left: m.width * 0.1,
         top: m.height * 0.1,
@@ -25,6 +24,6 @@ srcBg.metadata().then(function (m) {
         height: m.height * 0.8
     };
 
-    srcBg.clone().extract(extractOptions).resize(768).jpeg(bgOptions).toFile(path.join(imagePath, 'bg-768.jpg'));
-    srcBg.clone().extract(extractOptions).resize(576).jpeg(bgOptions).toFile(path.join(imagePath, 'bg-576.jpg'));
+    bgImg.clone().extract(extractOptions).resize(768).toFile(path.join(imgOutPath, 'bg-768.jpg'));
+    bgImg.clone().extract(extractOptions).resize(576).toFile(path.join(imgOutPath, 'bg-576.jpg'));
 });
